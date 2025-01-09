5fd68eb88b1c73a1b47cbb2ed7cc80d0c370c102
Anamika Aggarwal <anamikaagg18@gmail.com>
2025-01-09 11:12:07 +0530

    add transport, producer, consumer

--- server/src/mediasoupConnection.js
+++ server/src/mediasoupConnection.js
@@ -1,7 +1,11 @@
 import mediasoup from "mediasoup";
 
 let worker;
+let rooms = {};
+let peers = {}
 let transports = []
+let producers = []
+let consumers = []
 
 const createWorker = async () => {
     worker = await mediasoup.createWorker({
@@ -44,7 +48,7 @@ const createWebRtcTransport = async (router, socketId) => {
             const webRtcTransport_options = {
                 listenIps: [
                     {
-                        ip: '0.0.0.0', 
+                        ip: '0.0.0.0',
                         announcedIp: '192.168.1.14',
                     }
                 ],
@@ -110,6 +114,106 @@ const mediaSoupSocketConnection = (connections) => {
     connections.on('connection', async socket => {
         console.log('MediaSoup peer connected:', socket.id);
 
+        socket.on('joinRoom', async ({ roomName }, callback) => {
+            // create Router if it does not exist
+            // const router1 = rooms[roomName] && rooms[roomName].get('data').router || await createRoom(roomName, socket.id)
+            const router1 = await createRoom(roomName, socket.id)
+
+            peers[socket.id] = {
+                socket,
+                roomName,           // Name for the Router this Peer joined
+                transports: [],
+                producers: [],
+                consumers: [],
+                peerDetails: {
+                    name: '',
+                    isAdmin: false,   // Is this Peer the Admin?
+                }
+            }
+
+            // get Router RTP Capabilities
+            const rtpCapabilities = router1.rtpCapabilities
+
+            // call callback from the client and send back the rtpCapabilities
+            callback({ rtpCapabilities })
+        })
+
+        const addTransport = (transport, roomName, consumer) => {
+
+            transports = [
+                ...transports,
+                { socketId: socket.id, transport, roomName, consumer, }
+            ]
+
+            peers[socket.id] = {
+                ...peers[socket.id],
+                transports: [
+                    ...peers[socket.id].transports,
+                    transport.id,
+                ]
+            }
+        }
+
+        const addProducer = (producer, roomName) => {
+            producers = [
+                ...producers,
+                { socketId: socket.id, producer, roomName, }
+            ]
+
+            peers[socket.id] = {
+                ...peers[socket.id],
+                producers: [
+                    ...peers[socket.id].producers,
+                    producer.id,
+                ]
+            }
+        }
+
+        const addConsumer = (consumer, roomName) => {
+            // add the consumer to the consumers list
+            consumers = [
+                ...consumers,
+                { socketId: socket.id, consumer, roomName, }
+            ]
+
+            // add the consumer id to the peers list
+            peers[socket.id] = {
+                ...peers[socket.id],
+                consumers: [
+                    ...peers[socket.id].consumers,
+                    consumer.id,
+                ]
+            }
+        }
+
+        socket.on('createWebRtcTransport', async ({ consumer }, callback) => {
+            // get Room Name from Peer's properties
+            const roomName = peers[socket.id].roomName
+
+            // get Router (Room) object this peer is in based on RoomName
+            const router = rooms[roomName].router
+
+            console.log("in create webrtc transport event")
+
+            createWebRtcTransport(router, socket.id).then(
+                transport => {
+                    callback({
+                        params: {
+                            id: transport.id,
+                            iceParameters: transport.iceParameters,
+                            iceCandidates: transport.iceCandidates,
+                            dtlsParameters: transport.dtlsParameters,
+                        }
+                    })
+
+                    // add transport to Peer's properties
+                    addTransport(transport, roomName, consumer)
+                },
+                error => {
+                    console.log(error)
+                })
+        })
+        
         socket.emit('connection-success', {
             socketId: socket.id
         });
